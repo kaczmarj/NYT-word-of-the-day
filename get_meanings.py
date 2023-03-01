837698e358a9b553f9ecedc1c927bbbf82412234
Jakub Kaczmarzyk <jakub.kaczmarzyk@gmail.com>
2023-02-28 19:04:40 -0500

    use concurrent.futures to speed up get meanings

--- get_meanings.py
+++ get_meanings.py
@@ -1,17 +1,26 @@
 """Get the meanings of all words in 'words.txt' and save to JSON."""
 
+import concurrent.futures
 import json
 
 from PyDictionary import PyDictionary
+from tqdm import tqdm
+
+dictionary = PyDictionary()
+
+
+def get_meaning(term: str):
+    return (term, dictionary.meaning(term))
 
 
 def main():
     with open("words.txt") as f:
         words = [line.strip() for line in f]
 
-    print(len(words), "words")
-    dictionary = PyDictionary(words)
-    meanings = dictionary.getMeanings()
+    print(f"Gettings meanings of {len(words):,} words")
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        meanings = dict(tqdm(executor.map(get_meaning, words), total=len(words)))
 
     with open("meanings.json", "w") as f:
         json.dump(meanings, f)
